d991824d9bf41c0bcdc9d0495678653fdd4bbce3
cottonatocd <cotton@ocdagency.com>
2021-01-24 19:31:23 -0500

    subnav

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,44 @@ for (btn of navBtns){
     btn.addEventListener("click", function(){
         let activeBtn = document.querySelector("nav button.active");
         if (activeBtn != null && activeBtn != this){
-            document.querySelector("nav button.active").classList.remove("active")
+            document.querySelector("nav button.active").classList.remove("active");
+            nav.classList.remove("subnav-open");
         }
         this.classList.toggle("active");
+        nav.classList.toggle("subnav-open");
 
         if(nav.querySelectorAll("button.active").length == 1){
-            nav.classList.remove("lg")
+            nav.classList.remove("lg");
         } else if (scrollTop < 100){
-            nav.classList.add("lg")
+            nav.classList.add("lg");
         }
 
     })
 }
 
-window.onscroll=function(){
-    scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    if (scrollTop >=100){
-        nav.classList.remove("lg");
-    } else {
-        nav.classList.add("lg");
-    }
-}
 
-let randomPattern = Math.ceil(Math.random()*3);
-document.getElementById("custom").style.backgroundImage="url(/assets/img/pattern"+randomPattern+".jpg)";
+
+// HOMEPAGE
+
+if (document.body.id == "homepage"){
+
+    let randomPattern = Math.ceil(Math.random()*3);
+    document.getElementById("custom").style.backgroundImage="url(/assets/img/pattern"+randomPattern+".jpg)";
+
+    // scroll nav
+    window.onscroll=function(){
+        scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        if (scrollTop >=100){
+            nav.classList.remove("lg");
+        } else {
+            nav.classList.add("lg");
+        }
+    }
+
+
+
+} else {
+        
+    nav.classList.remove("lg");
+
+}
