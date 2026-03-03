5bfa8a0a7d647bcd66242c20369ddc5fbbbe7e71
Jennifer Galas <jennifer.galas@scranton.edu>
2026-03-02 21:28:06 -0500

    Update BBW slideshow script. move to separate file

--- assets/js/bbw-image-grid.js
+++ assets/js/bbw-image-grid.js
@@ -1,34 +1,32 @@
 let gridImgs = document.querySelectorAll(".bbw-image-grid img");
 let captions = document.querySelectorAll(".bbw-image-grid + figcaption span");
-
+let showCount = Math.min(6, gridImgs.length);
 var arr = [];
 
-function shuffleGrid(){
+function shuffleGrid() {
 
-    arr = [];
-    while(arr.length < 6){
-        var r = Math.floor(Math.random() * gridImgs.length) + 1;
-        if(arr.indexOf(r) === -1) arr.push(r);
-    }
+  arr = [];
+  while (arr.length < showCount) {
+    var r = Math.floor(Math.random() * gridImgs.length) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
 
-    for (let i=0;i<gridImgs.length;i++){
-        captions[i].classList.remove("last");
-        if (!arr.includes(i+1)){
-            gridImgs[i].style.display="none";
-            captions[i].style.display="none";
-        } else {
-            gridImgs[i].style.display="block";
-            captions[i].style.display="inline-block";
-            if (i+1 == Math.max.apply(Math, arr)){
-                captions[i].classList.add("last");
-            }
-        }
+  for (let i = 0; i < gridImgs.length; i++) {
+    captions[i].classList.remove("last");
+    if (!arr.includes(i + 1)) {
+      gridImgs[i].style.display = "none";
+      captions[i].style.display = "none";
+    } else {
+      gridImgs[i].style.display = "block";
+      captions[i].style.display = "inline-block";
+      if (i + 1 == Math.max.apply(Math, arr)) {
+        captions[i].classList.add("last");
+      }
     }
+  }
 
 }
 shuffleGrid();
-setInterval(function(){
-    shuffleGrid();
-},3000)
-
-
+setInterval(function () {
+  shuffleGrid();
+}, 3000)
